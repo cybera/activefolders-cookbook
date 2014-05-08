d4291105e19eb485b77361eaee30d8ffde10ab02
alex <alex.valiushko@cybera.ca>
2014-05-08 14:37:52 -0600

    typo

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
-file_cache_path "/home/ubuntu/activefolders-chef/.chef"
-cookbook_path "/home/ubuntu/activefolders-chef/cookbooks"
-json_attribs "/home/ubuntu/activefolders-chef/server-node.json"
+file_cache_path "/home/ubuntu/activefolders-cookbook/.chef"
+cookbook_path "/home/ubuntu/activefolders-cookbook/cookbooks"
+json_attribs "/home/ubuntu/activefolders-cookbook/server-node.json"
